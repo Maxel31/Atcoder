43d730567a4a86c3dc0b190b8a2d2858199921f8
Maxel31 <sorutongenuine.31@gmail.com>
2023-08-19 23:36:06 +0900

    [fix] 315

--- ABC/315/E.cpp
+++ ABC/315/E.cpp
@@ -1,31 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-// using P = pair<int, int>;
-const long long INF = 1LL<<60;
 
-int main(){
-    // 入力
-	int N;
-	cin >> N;
-	vector<vector<int>>G(N+1);
-	for(int i=1;i<=N;i++){
-		int c;
-		cin >> c;
-		G[i].resize(c);
-		for(auto&v:G[i])cin >> v;
-	}
+const long long INF = 1LL << 60;
 
-	vector<bool>visited(N+1);
-	vector<int>ans;
-    
-	auto dfs=[&](auto self,int v)->void{
-		for(auto vv:G[v])if(!visited[vv])self(self,vv);
-		ans.push_back(v);
-	    visited[v]=true;
-	};
-	dfs(dfs,1);
-	ans.pop_back();  // 1 自身は除く
-	for(auto v:ans)cout << v << " ";
+vector<vector<int>> G;
+vector<bool> visited;
+vector<int> ans;
+
+void dfs(int v) {
+    visited[v] = true;
+    for (auto vv : G[v]) {
+        if (!visited[vv]) {
+            dfs(vv);
+        }
+    }
+    ans.push_back(v);
+}
+
+int main() {
+	// 入力
+    int N;
+    cin >> N;
+
+    G.resize(N + 1);
+    visited.resize(N + 1, false); // initialize all vertices as not visited
+
+	// 有効グラフの作成
+    for (int i = 1; i <= N; i++) {
+        int c;
+        cin >> c;
+        G[i].resize(c);
+        for (auto &v : G[i]) cin >> v;
+    }
+
+	// 処理,出力
+    dfs(1);
+    ans.pop_back(); // 1 自身は除く
+    for (auto v : ans) cout << v << " ";
     cout << endl;
 }
