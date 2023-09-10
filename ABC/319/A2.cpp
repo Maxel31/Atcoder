db64b3d9d182df3803ec9655d511eff913f500b4
Maxel31 <sorutongenuine.31@gmail.com>
2023-09-10 14:13:06 +0900

    [add] ABC319/A2.cpp

---
+++ ABC/319/A2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+# define rep(i, n) for(int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+const long long INF = 1LL << 60;
+
+
+// 問題文コピペしてコード書く用
+int main(){
+    for(int i = 0; i < 10; i++){
+        string s, x;
+        cin >> s >> x;
+        cout << "    if(s==\"" << s << "\") cout << " << x << " << endl;\n";
+
+    }
+}
